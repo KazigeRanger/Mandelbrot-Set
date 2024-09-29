58e69ea837ed5fecb467fc18ba765d8739b09189
Renger Bierenga <bierengarenger@gmail.com>
2024-09-29 23:17:42 +0200

    Added the equation that describes the mandelbrot set and all the necessary logic.

--- Mandelbrot Point Generation/pointGenerator.js
+++ Mandelbrot Point Generation/pointGenerator.js
@@ -3,8 +3,24 @@ const { generateLinSpace } = require("./generateLinearSpaces.js");
 
 function generateMandelbrotPoints(maxNumberOfIterations, resolution) {
     let z = new complexNumber(0, 0);
-    const imageRange = generateLinSpace(-2.5, 1, resolution);
-    console.log(imageRange);
+    const imageHorizontalRange = generateLinSpace(-2.5, 1, resolution);
+    const imageVerticalRange = generateLinSpace(-1, 1, resolution);
+
+    console.log("CHECK CHECK");
+
+    let iterationCount = 0;
+    for (let i = 0; i < imageHorizontalRange.length; i++) {
+        for (let j = 0; j < imageVerticalRange.length; j++) {
+            let c = new complexNumber(imageHorizontalRange[i], imageVerticalRange[j]);
+
+            for (let k = 0; k < maxNumberOfIterations; k++) {
+                iterationCount += 1
+                console.log(`ITERATION ${iterationCount}: z = ${z._realPart} + ${z._imaginaryPart}i     c = ${c._realPart} + ${c._imaginaryPart}i`);
+                z1 = z.multiplyComplex(z);
+                z = z1.add(c);
+            }
+        }
+    }
 }
 
-generateMandelbrotPoints(10, 100)
+generateMandelbrotPoints(5, 11);
